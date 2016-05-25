c040d1735787b643c3aca708a89f2294d349bca1
Viren Nadkarni <viren@outlook.com>
2016-05-25 16:41:34 +0530

    implement search page and stuff

--- play/urls.py
+++ play/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^app/(.*)/$', views.app, name='app'),
+    url(r'^results/$', views.results, name='results'),
 ]
--- play/views.py
+++ play/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import PlayApp
 
 
-# Create your views here.
-def index(requets):
-    return HttpResponse("hello world")
+def search(requests): 
+    template = loader.get_template('search.html')
+    return HttpResponse(template.render(template))
+
+
+def results(requests): 
+    template = loader.get_template('results.html')
+    if requests.method == 'GET':
+        search_term = requests.GET.get('search_term')
+
+    # search database for the term
+    try:
+        output = PlayApp.objects.get(app_name__contains=search_term)
+    except:
+        # else web scrape
+        pass
+        # and cache in db
+
+    return HttpResponse(output)
+
+
+def app(requests, entry_name): 
+    output = entry_name
+    return HttpResponse(output)
+
