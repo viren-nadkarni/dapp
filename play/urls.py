c76ce021d91f367762c1833dfd5d9eb6460c43cd
Viren Nadkarni <viren@outlook.com>
2016-05-26 17:45:15 +0530

    use a more sensible routing

--- play/urls.py
+++ play/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^search/$', views.search, name='search'),
-    url(r'^app/(.*)/$', views.app, name='app'),
+    url(r'^$', views.search, name='search'),
+    url(r'^results/(.*)/$', views.app, name='app'),
     url(r'^results/$', views.results, name='results'),
 ]
