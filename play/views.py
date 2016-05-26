c5e7cbbda3e502602e9c46fe2d0f879f1ac92554
Viren Nadkarni <viren@outlook.com>
2016-05-26 17:30:36 +0530

    implement search

--- play/views.py
+++ play/views.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import lxml.html
 
@@ -12,53 +13,36 @@ APP_SEARCH_URL = 'https://play.google.com/store/search?c=apps&q={}'
 APP_DETAILS_URL = 'https://play.google.com/store/apps/details?id={}'
 
 
-def search(request): 
-    template = loader.get_template('search.html')
-    return HttpResponse(template.render(template))
-
-
-def results(request): 
-    template = loader.get_template('results.html')
-    if request.method == 'GET':
-        search_term = request.GET.get('search_term')
-
-    # search database for the term
-    try:
-        output = PlayApp.objects.filter(app_name__contains=search_term)
-    except:
-        # else web scrape
-        pass
-
-    return HttpResponse( template.render( context={'search_results': output} ) )
-
-
-def app(request, app_id): 
-    template = loader.get_template('app.html')
-
+def get_app_details(app_id):
     try:
         # check if the said app details are available in db
-        output = PlayApp.objects.get(app_id__iexact=app_id)
+        # get it as a dict object
+        output = PlayApp.objects.get(app_id__iexact=app_id).__dict__
 
         # the source field will indicate where the details have been pulled from
-        source = 'cache'
+        output['source'] = 'cache'
 
     except ObjectDoesNotExist:
         # app details are not cached
         # so we scrape app info
         store_page = requests.get( APP_DETAILS_URL.format( app_id ) ).text
         tree = lxml.html.fromstring(store_page)
-        source = 'web'
 
-        # the XPath has been copied from the browser inspection mode
-        # susceptible to failure even if a small change is made in page layout
+        # XPath might be susceptible to failure even if a change is made in page layout
         try:
+            # create a dict object representing the app
             output = {
                 'app_id': app_id,
-                'app_name': tree.xpath('//*[@id="body-content"]/div/div/div[1]/div[1]/div/div[1]/div[2]/div[1]/div/text()')[0],
-                'dev_name': tree.xpath('//*[@id="body-content"]/div/div/div[1]/div[3]/div/div[2]/div[8]/div[2]/text()')[0],
-                'dev_email': tree.xpath('//*[@id="body-content"]/div/div/div[1]/div[3]/div/div[2]/div[9]/div[2]/a[2]/@href')[0],
-                'icon_url': tree.xpath('//*[@id="body-content"]/div/div/div[1]/div[1]/div/div[1]/div[1]/img/@src')[0],
+                'app_name': tree.xpath('//div[@class="id-app-title"]/text()')[0],
+                'dev_name': tree.xpath('//div[div[@class="title"] = "Offered By"]/div[@class="content"]/text()')[0],
+                'icon_url': tree.xpath('//img[@class="cover-image"]/@src')[0],
+                'source': 'web',
             }
+
+            # some apps have details like Privacy Policy, Mailing address, some don't. Handle it here
+            # no particular order is followed, 
+            dev_email_value = tree.xpath('//a[@class="dev-link"]/@href')
+            output['dev_email'] = [email for email in dev_email_value if 'mailto:' in email][0][7:]
 
             # and cache it in db
             PlayApp(app_id=output['app_id'],
@@ -69,15 +53,45 @@ def app(request, app_id):
 
         except IndexError:
             # exception when the app does not exist on play store
+            print sys.exc_info()
             output = {
                 'app_id': app_id,
                 'app_name': 'Not found',
                 'dev_name': 'Not found',
                 'dev_email': '',
                 'icon_url': '',
+                'source': 'web',
             }
 
+    return output
+
+
+
+def search(request): 
+    template = loader.get_template('search.html')
+    return HttpResponse(template.render(template))
+
+
+def results(request): 
+    template = loader.get_template('results.html')
+    search_term = request.GET.get('search_term')
+
+    store_results_raw = requests.get( APP_SEARCH_URL.format(search_term) ).text
+    tree = lxml.html.fromstring(store_results_raw)
+    results = tree.xpath('//div[@class="card-content id-track-click id-track-impression"]/@data-docid')
+
+    output = list()
+    for app_id in results[:10]:
+        output.append( get_app_details(app_id) )
+
+    return HttpResponse( template.render( context={'search_results': output} ) )
+
+
+def app(request, app_id): 
+    template = loader.get_template('app.html')
+
+    output = get_app_details(app_id)
+
     return HttpResponse( template.render( context={'app': output,
-                                                   'source': source,
                                                    'request': request} ) )
 
